82b603bfb86970bbb34f9be3d8ff02cc4971dc5b
varun kaushik <varun0509kaushik@gmail.com>
2024-07-24 11:14:46 +0530

    Added Pagination and Sorting

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,20 +19,36 @@ router.post('/tasks',auth, async (req,res)=>{
 })
 
 // GET /tasks?completed=?
+// GET /tasks?limit=10&skip=0 ->this is called as pagination
+// GET /taks?sortBy=createdAt:desc
 router.get('/tasks',auth,async (req,res)=>{
     const match={}
+    const sort={}//Ye 2 lines isliye ki user ne url mai kuch specify hi nhi kia
 
     if(req.query.completed) {
         match.completed=req.query.completed==='true'
     }
 
+    if(req.query.sortBy) {
+        const parts= req.query.sortBy.split(':')//we can use any special char like _ or :
+        sort[parts[0]]=parts[1] === 'desc' ? -1 : 1
+    }
     try{
         //const tasks=await Task.find({owner:req.user._id})
         //res.send(tasks) 
         //both the methods works
         await req.user.populate({
             path:'tasks',
-            match
+            match,
+            options:{
+                limit:parseInt(req.query.limit),
+                skip:parseInt(req.query.skip),
+                sort
+                /*sort:{
+                    //createdAt:1//for desc we use -1
+                    //completed:1
+                }*/
+            }
         }).execPopulate()
         res.send(req.user.tasks)
     } catch(e) {
