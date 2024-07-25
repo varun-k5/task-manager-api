01b05d77fcf73a4589d00e113dacb347a5023001
varun kaushik <varun0509kaushik@gmail.com>
2024-07-25 13:55:11 +0530

    Added support for file uploads

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,7 @@ const express=require('express')
 const User=require('../models/user')
 const auth=require('../middleware/auth')
 const router=new express.Router()
+const multer=require('multer')
 
 router.post('/users', async (req,res)=>{
     const user=new User(req.body)
@@ -119,5 +120,24 @@ router.delete('/users/me',auth,async(req,res)=>{
         res.status(500).send()
     }
 })*/
+
+const upload=multer({
+    dest:'avatars',
+    limits:{
+        fileSize:1000000//bytes mai 
+    },
+    fileFilter(req, file, cb) {//cb mtlb callback
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image'))
+        }
+        
+        cb(undefined, true)
+
+    }
+})
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res)=>{
+    res.send()
+})
 
 module.exports=router
