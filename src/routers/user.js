9ef41bcf51a29c74f477cbea23dafdae61c53433
varun kaushik <varun0509kaushik@gmail.com>
2024-07-25 17:12:08 +0530

    Adding Image to user profile

--- src/routers/user.js
+++ src/routers/user.js
@@ -122,7 +122,6 @@ router.delete('/users/me',auth,async(req,res)=>{
 })*/
 
 const upload=multer({
-    dest:'avatars',
     limits:{
         fileSize:1000000//bytes mai 
     },
@@ -136,8 +135,33 @@ const upload=multer({
     }
 })
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res)=>{
+router.post('/users/me/avatar',auth, upload.single('avatar'), async (req, res)=>{
+    req.user.avatar=req.file.buffer
+    await req.user.save()
     res.send()
+},(error,req,res,next)=>{
+    res.status(400).send({error:error.message})
+})
+
+router.delete('/users/me/avatar',auth, async(req,res)=>{
+    req.user.avatar=undefined
+    await req.user.save()
+    res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res)=>{//Not added in postman
+    try{
+        const user= await User.findById(req.params.id)
+
+        if ( !user || !user.avatar) {
+            throw new error()
+        }
+
+        res.set('Content-Type', 'image/jpg')//to set the header
+        res.send(user.avatar)
+    } catch(e) {
+        res.status(404).send()
+    }
 })
 
 module.exports=router
